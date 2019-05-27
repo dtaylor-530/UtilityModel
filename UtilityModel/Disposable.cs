0854fb3b647bd9ddfd73c22d2c86188c6353d2e4
dtaylor-530 <d.taylor530@gmail.com>
2019-05-27 13:20:07 +0100

    Initial commit

---
+++ UtilityModel/Disposable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityModel
+{
+    public class Disposable : IDisposable
+    {
+     
+        private bool isDisposed = false;
+        public void Dispose() { Dispose(true); GC.SuppressFinalize(this); }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed)
+            {
+                if (disposing)
+                {
+                    //    if (_wplayer != null) _wplayer.Dispose();
+                }
+            }
+            isDisposed = true;
+        }
+  
+    }
+}
